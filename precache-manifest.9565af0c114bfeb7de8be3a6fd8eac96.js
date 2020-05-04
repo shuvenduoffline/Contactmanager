e0380f17aa00549ccc826012ff6ae7b7db7a40cb
Shuvendu Dhal <shuveduoffline@gmail.com>
2020-05-04 19:27:46 +0530

    Updates

---
+++ precache-manifest.9565af0c114bfeb7de8be3a6fd8eac96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e847971176d3dbfc1112b33fec670c9",
+    "url": "/Contactmanager/index.html"
+  },
+  {
+    "revision": "3174d1e51370b2e5ea86",
+    "url": "/Contactmanager/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "e86e8957a50ee6612cda",
+    "url": "/Contactmanager/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3174d1e51370b2e5ea86",
+    "url": "/Contactmanager/static/js/2.c89adce3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Contactmanager/static/js/2.c89adce3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e86e8957a50ee6612cda",
+    "url": "/Contactmanager/static/js/main.d9ae8faf.chunk.js"
+  },
+  {
+    "revision": "a138379f9736d6d4d161",
+    "url": "/Contactmanager/static/js/runtime-main.518b4da0.js"
+  }
+]);
